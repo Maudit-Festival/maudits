818d0cf2fc58a10f120766957fe281adf57b00ed
JA <jagw40k@free.fr>
2022-12-30 15:22:53 +0100

    Use GetMapper

--- src/main/java/com/maudits/website/controller/SiteMapController.java
+++ src/main/java/com/maudits/website/controller/SiteMapController.java
@@ -1,8 +1,7 @@
 package com.maudits.website.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.maudits.website.domain.sitemap.Sitemap;
@@ -15,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class SiteMapController {
 	private final SitemapService sitemapService;
 
-	@RequestMapping(value = "/sitemap.xml", method = RequestMethod.GET)
+	@GetMapping("/sitemap.xml")
 	@ResponseBody
 	public Sitemap main() {
 		return sitemapService.buildSitemap();
